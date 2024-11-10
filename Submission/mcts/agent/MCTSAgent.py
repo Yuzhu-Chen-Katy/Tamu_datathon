268b366aecd572a81454e97db5e77c2ca641bd86
Aurora Jitrskul <aurawra@outlook.com>
2024-11-10 12:14:43 -0600

    Refactor codebase again

---
+++ Submission/mcts/agent/MCTSAgent.py
@@ -0,0 +1,12 @@
+from mcts.searcher.mcts import MCTS
+from mcts.base.base import BaseState
+
+
+class MCTSAgent():
+  def __init__(self):
+    self.searcher = MCTS(3800)
+  
+  def get_best_move(self, game):
+    currentState = BaseState(game)
+    best_move = self.searcher.search(initial_state=currentState, turnCount=game.turn_count)
+    return best_move
